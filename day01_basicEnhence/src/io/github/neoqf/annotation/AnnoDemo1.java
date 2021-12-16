d88a991ac52435e8409729f16df392aae1a334b2
LQF <liuqifeng19960918@outlook.com>
2021-12-16 22:30:06 +0800

    反射案例

---
+++ day01_basicEnhence/src/io/github/neoqf/annotation/AnnoDemo1.java
@@ -0,0 +1,21 @@
+package io.github.neoqf.annotation;
+
+/**
+ * 注解javadoc演示
+ *
+ * @author LQF
+ * @version 1.0
+ * @since JDK1.5
+ */
+public class AnnoDemo1 {
+
+    /**
+     * 计算两数的和
+     * @param a 整数
+     * @param b 整数
+     * @return 两数之和
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
