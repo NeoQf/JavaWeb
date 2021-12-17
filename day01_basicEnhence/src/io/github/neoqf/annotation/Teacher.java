9e5b9ebb327bf27e8811da2160521e7959393737
LQF <liuqifeng19960918@outlook.com>
2021-12-18 00:10:59 +0800

    注解

---
+++ day01_basicEnhence/src/io/github/neoqf/annotation/Teacher.java
@@ -0,0 +1,5 @@
+package io.github.neoqf.annotation;
+
+public class Teacher extends  Worker {
+
+}
--- day01_basicEnhence/src/io/github/neoqf/annotation/Worker.java
+++ day01_basicEnhence/src/io/github/neoqf/annotation/Worker.java
@@ -1,6 +1,6 @@
 package io.github.neoqf.annotation;
 
-@MyAnno(value = 12, show2 = "", per = Person.P3, anno2 = @MyAnno2, strs = {"bbb", "aaa"})
+@MyAnno(value = 12, show2 = " ", per = Person.P3, anno2 = @MyAnno2, strs = {"bbb", "aaa"})
 @MyAnno3
 public class Worker {
     @MyAnno3
